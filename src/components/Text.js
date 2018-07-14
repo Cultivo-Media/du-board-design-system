81aa9c0666f988e6361026a3a0f7a2d7808f37d3
Henry Kaufman <hkaufman19@gmail.com>
2018-07-14 07:22:29 -1000

    :package: Add Text component and update defualt exports

---
+++ src/components/Text.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { colors } from '../variables';
+
+export const SectionHeader = styled.h5`
+  color: ${colors.gray};
+  font-size: 12px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`;
+
+export const DefaultLabel = styled.span`
+  color: ${colors.darkGray} !important;
+  font-size: 16px !important;
+  margin-left: 8px;
+`;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ export { CardTableRow } from './components/CardTable';
 export { CardTableRowSub } from './components/CardTable';
 export { CardTableRowHead } from './components/CardTable';
 
+// Checkbox
+export { default as MappedCheckbox } from './components/CheckBox';
+
 // Flex
 export { Flex } from './components/Flex';
 export { Grow } from './components/Flex';
+
+// Header
+export { default as Header } from './components/Header';
+
+// Navbar
+export { default as Navbar } from './components/Navbar';
+
+// Text
+export { SectionHeader, DefaultLabel } from './components/Text';
