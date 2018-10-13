a7bcbc448b4d91af96da7fef02614371087db7d5
Henry Kaufman <hkaufman19@gmail.com>
2018-10-13 15:37:38 -0400

    Fix typo in the Button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,7 +13,7 @@ export default styled.button`
   background-color: ${({ primary, theme }) => (primary ? (theme.blue || colors.blue) : 'transparent')};
   border: none;
   border-radius: 4px;
-  color: ${({ primary, theme }) => (primary ? (theme.white || colors.white) : colors.gray)};
+  color: ${({ primary, theme }) => (primary ? (theme.white || colors.white) : (theme.gray || colors.gray))};
   cursor: pointer;
   font-size: 16px;
   font-weight: 500;
